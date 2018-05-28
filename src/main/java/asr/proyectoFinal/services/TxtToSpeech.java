a06d11ccbfff5f5bfbb26071df2d59e30b300ca8
Begoña de Miguel <201303743@alu.comillas.edu>
2018-05-28 12:00:48 +0200

    t

--- src/main/java/asr/proyectoFinal/services/TxtToSpeech.java
+++ src/main/java/asr/proyectoFinal/services/TxtToSpeech.java
@@ -14,7 +14,7 @@ import com.google.gson.JsonParser;
 import com.ibm.watson.developer_cloud.language_translator.v2.LanguageTranslator;
 import com.ibm.watson.developer_cloud.language_translator.v2.util.Language;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
-import com.ibm.watson.developer_cloud.text_to_speech.v1.model.AudioFormat;
+//import com.ibm.watson.developer_cloud.text_to_speech.v1.model.AudioFormat;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.util.WaveUtils;
 import com.ibm.watson.developer_cloud.language_translator.v2.model.TranslateOptions.Builder;
@@ -30,6 +30,9 @@ public class TxtToSpeech
 	{
 		
 		//TextToSpeech textService = new TextToSpeech(USER_NAME, PASSWORD);
+
+		//Voice voces = textService.listVoices().execute();
+		
 		
 		//List<Voice> voices = service.getVoices().execute();
 		//System.out.println(voices);
